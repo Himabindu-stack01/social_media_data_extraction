be17ba6afc4d26ed3ba6c31f43dd9ffcd74517f6
hima bindu <109454411+Himabindu-stack01@users.noreply.github.com>
2025-08-19 13:00:01 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-DATABASE_URL = "postgresql://postgres:Bindu%40123@localhost:5432/socialmedia"
+# ✅ Update with your credentials
+DATABASE_URL = "postgresql://postgres:Bindu%40123@localhost/socialmedia"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Dependency for FastAPI routes
 def get_db():
     db = SessionLocal()
     try:
